38030d38ad75af09a37d8a54520beaac66baae67
H.-Peter Pfeufer <info@ppfeufer.de>
2017-09-09 09:13:18 +0200

    Nesseccary files added

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Our Theme's namespace to keep the global namespace clear
+ *
+ * WordPress\Themes\YulaiFederation
+ */
+namespace WordPress\Themes\Ppfeufer;
+
+require_once(\trailingslashit(\dirname(__FILE__)) . 'inc/autoloader.php');
