f1cf942a6b7815d6d66dbc84af8a6f2c42e776f2
Peter Pfeufer <p.pfeufer@thamm-it.de>
2019-07-10 13:29:22 +0200

    Templates reorganized

--- index.php
+++ index.php
@@ -16,3 +16,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+defined('ABSPATH') or die();
+
+get_header();
+?>
+
+<?php
+get_footer();
