1cacb6edc6885c336925f0ba5f95b96d44f85736
Peter Pfeufer <p.pfeufer@thamm-it.de>
2019-07-10 11:23:54 +0200

    Windows sucks ...

--- templates/sidebars/sidebar-post.php
+++
@@ -1,9 +0,0 @@
-<?php defined('ABSPATH') or die(); ?>
-
-<section class="sidebar-posts">
-	<?php
-	if(\function_exists('\dynamic_sidebar')) {
-		\dynamic_sidebar('sidebar-post');
-	} // END if(function_exists('dynamic_sidebar'))
-	?>
-</section>
