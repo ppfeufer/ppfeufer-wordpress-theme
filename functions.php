4cd4c3ab5b38d1998936f8c075ce46226f9c9b35
Peter Pfeufer <info@ppfeufer.de>
2022-03-22 14:41:06 +0100

    Redirect to my favicon instead of the WP logo ..
    Thanks WordPress, this change is fucking stupid

--- functions.php
+++ functions.php
@@ -10,6 +10,28 @@ function ppfeufer_enqueue_styles() {
 
 add_action('wp_enqueue_scripts', 'ppfeufer_enqueue_styles');
 
+
+/**
+ * Redirect to the right favicon.ico
+ *
+ * WordPress redirects to a default favicon (admin_url('images/w-logo-blue.png')) since v5.5, which is not what I want
+ * so I have to do it myself here ...
+ *
+ * https://ppfeufer.de/favicon.ico will be redirected to get_theme_file_uri('/favicons/favicon.ico')
+ *
+ * @see https://make.wordpress.org/core/2020/02/19/enhancements-to-favicon-handling-in-wordpress-5-4/
+ *
+ * @return void
+ */
+function ppfeufer_favicon_ico() {
+    wp_redirect(get_site_icon_url(32, get_theme_file_uri('/favicons/favicon.ico')));
+
+    exit;
+}
+
+add_action( 'do_faviconico', 'ppfeufer_favicon_ico');
+
+
 /**
  * Adding favicons
  *
