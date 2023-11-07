f938c44bed496cabc608531601e2e6e91c87b298
Peter Pfeufer <info@ppfeufer.de>
2023-11-07 07:08:38 +0100

    [CHANGE] Move favicons to Assets directory

--- functions.php
+++ functions.php
@@ -137,7 +137,7 @@ add_action(
  * WordPress redirects to a default favicon (admin_url('images/w-logo-blue.png')) since v5.5, which is not what I want,
  * so I have to do it myself here ...
  *
- * https://ppfeufer.de/favicon.ico will be redirected to get_theme_file_uri('/favicons/favicon.ico')
+ * https://ppfeufer.de/favicon.ico will be redirected to get_theme_file_uri('/Assets/favicons/favicon.ico')
  *
  * @see https://make.wordpress.org/core/2020/02/19/enhancements-to-favicon-handling-in-wordpress-5-4/
  *
@@ -145,7 +145,7 @@ add_action(
  */
 function ppfeufer_favicon_ico(): void {
     wp_redirect(location: get_site_icon_url(
-        size: 32, url: get_theme_file_uri('/favicons/favicon.ico')
+        size: 32, url: get_theme_file_uri('/Assets/favicons/favicon.ico')
     ));
 
     exit;
@@ -159,24 +159,24 @@ add_action(hook_name: 'do_faviconico', callback: 'ppfeufer_favicon_ico');
  * @return void
  */
 function ppfeufer_favicons(): void {
-    echo '<link rel="apple-touch-icon" sizes="180x180" href="' . get_stylesheet_directory_uri() . '/favicons/apple-touch-icon.png">' . "\n";
-    echo '<link rel="icon" type="image/png" sizes="32x32" href="' . get_stylesheet_directory_uri() . '/favicons/favicon-32x32.png">' . "\n";
-    echo '<link rel="icon" type="image/png" sizes="192x192" href="' . get_stylesheet_directory_uri() . '/favicons/android-chrome-192x192.png">' . "\n";
-    echo '<link rel="icon" type="image/png" sizes="16x16" href="' . get_stylesheet_directory_uri() . '/favicons/favicon-16x16.png">' . "\n";
-    echo '<link rel="manifest" href="' . get_stylesheet_directory_uri() . '/favicons/site.webmanifest">' . "\n";
-    echo '<link rel="shortcut icon" href="' . get_stylesheet_directory_uri() . '/favicons/favicon.ico">' . "\n";
+    echo '<link rel="apple-touch-icon" sizes="180x180" href="' . get_stylesheet_directory_uri() . '/Assets/favicons/apple-touch-icon.png">' . "\n";
+    echo '<link rel="icon" type="image/png" sizes="32x32" href="' . get_stylesheet_directory_uri() . '/Assets/favicons/favicon-32x32.png">' . "\n";
+    echo '<link rel="icon" type="image/png" sizes="192x192" href="' . get_stylesheet_directory_uri() . '/Assets/favicons/android-chrome-192x192.png">' . "\n";
+    echo '<link rel="icon" type="image/png" sizes="16x16" href="' . get_stylesheet_directory_uri() . '/Assets/favicons/favicon-16x16.png">' . "\n";
+    echo '<link rel="manifest" href="' . get_stylesheet_directory_uri() . '/Assets/favicons/site.webmanifest">' . "\n";
+    echo '<link rel="shortcut icon" href="' . get_stylesheet_directory_uri() . '/Assets/favicons/favicon.ico">' . "\n";
 
     echo __create_meta_tag(
             property: 'msapplication-TileColor', content: '#da532c', type: 'name'
         ) . "\n";
     echo __create_meta_tag(
             property: 'msapplication-TileImage',
-            content: get_stylesheet_directory_uri() . '/favicons/mstile-144x144.png',
+            content: get_stylesheet_directory_uri() . '/Assets/favicons/mstile-144x144.png',
             type: 'name'
         ) . "\n";
     echo __create_meta_tag(
             property: 'msapplication-config',
-            content: get_stylesheet_directory_uri() . '/favicons/browserconfig.xml',
+            content: get_stylesheet_directory_uri() . '/Assets/favicons/browserconfig.xml',
             type: 'name'
         ) . "\n";
     echo __create_meta_tag(
